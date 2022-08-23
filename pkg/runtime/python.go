a003fccdefb0b00c70487eaa43fd4e99b45f16d7
Tim Holm <tim.holm@nitric.io>
2022-08-23 12:57:06 +1000

    chore: wip python for nitric run.

--- pkg/runtime/python.go
+++ pkg/runtime/python.go
@@ -22,8 +22,9 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/strslice"
 	"github.com/nitrictech/boxygen/pkg/backend/dockerfile"
-	"github.com/nitrictech/cli/pkg/utils"
 )
 
 type python struct {
@@ -46,22 +47,86 @@ func (t *python) BuildIgnore() []string {
 }
 
 func (t *python) FunctionDockerfileForCodeAsConfig(w io.Writer) error {
-	return utils.NewNotSupportedErr("code-as-config not supported on " + string(t.rte))
+	css := dockerfile.NewStateStore()
+
+	con, err := css.NewContainer(dockerfile.NewContainerOpts{
+		From:   "python:3.9-slim",
+		As:     layerFinal,
+		Ignore: t.BuildIgnore(),
+	})
+	if err != nil {
+		return err
+	}
+
+	con.Run(dockerfile.RunOptions{Command: []string{"pip", "install", "--upgrade", "pip"}})
+	con.Config(dockerfile.ConfigOptions{
+		WorkingDir: "/",
+	})
+
+	con.Run(dockerfile.RunOptions{Command: []string{"pip", "install", "jurigged"}})
+
+	err = con.Copy(dockerfile.CopyOptions{Src: "requirements.txt", Dest: "requirements.txt"})
+	if err != nil {
+		return err
+	}
+
+	con.Run(dockerfile.RunOptions{Command: []string{"pip", "install", "--no-cache-dir", "-r", "requirements.txt"}})
+
+	err = con.Copy(dockerfile.CopyOptions{Src: ".", Dest: "."})
+	if err != nil {
+		return err
+	}
+
+	con.Config(dockerfile.ConfigOptions{
+		Env: map[string]string{
+			"PYTHONPATH": "/app/:${PYTHONPATH}",
+		},
+		Ports: []int32{9001},
+		Cmd:   []string{"python", t.handler},
+	})
+
+	_, err = w.Write([]byte(strings.Join(con.Lines(), "\n")))
+
+	return err
 }
 
 func (t *python) LaunchOptsForFunctionCollect(runCtx string) (LaunchOpts, error) {
-	return LaunchOpts{}, utils.NewNotSupportedErr("code-as-config not supported on " + string(t.rte))
+	return LaunchOpts{
+		Image:      t.DevImageName(),
+		Entrypoint: strslice.StrSlice{"python"},
+		Cmd:        strslice.StrSlice{"/app/" + filepath.ToSlash(t.handler)},
+		TargetWD:   "/app",
+		Mounts: []mount.Mount{
+			{
+				Type:   "bind",
+				Source: runCtx,
+				Target: "/app",
+			},
+		},
+	}, nil
 }
 
 func (t *python) LaunchOptsForFunction(runCtx string) (LaunchOpts, error) {
-	return LaunchOpts{}, utils.NewNotSupportedErr("code-as-config not supported on " + string(t.rte))
+	return LaunchOpts{
+		Image:      t.DevImageName(),
+		Entrypoint: strslice.StrSlice{"python"},
+		Cmd:        strslice.StrSlice{"-m", "jurigged", "-v", "/app/" + filepath.ToSlash(t.handler)},
+		TargetWD:   "/app",
+		Mounts: []mount.Mount{
+			{
+				Type:   "bind",
+				Source: runCtx,
+				Target: "/app",
+			},
+		},
+	}, nil
 }
 
 func (t *python) FunctionDockerfile(funcCtxDir, version, provider string, w io.Writer) error {
 	css := dockerfile.NewStateStore()
 
 	con, err := css.NewContainer(dockerfile.NewContainerOpts{
-		From:   "python:3.7-slim",
+		From:   "python:3.9-slim",
 		As:     layerFinal,
 		Ignore: t.BuildIgnore(),
 	})
